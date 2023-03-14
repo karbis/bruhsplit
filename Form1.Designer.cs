00278241098ecf9b2ad260baa07aae22e7f7e64b
karbis <102873508+karbis@users.noreply.github.com>
2023-03-14 16:41:52 +0200

    start

---
+++ Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace bruhsplit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DebugMsg = new System.Windows.Forms.Label();
+            this.TimerText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // DebugMsg
+            // 
+            this.DebugMsg.AutoSize = true;
+            this.DebugMsg.ForeColor = System.Drawing.Color.White;
+            this.DebugMsg.Location = new System.Drawing.Point(35, 85);
+            this.DebugMsg.Name = "DebugMsg";
+            this.DebugMsg.Size = new System.Drawing.Size(0, 13);
+            this.DebugMsg.TabIndex = 0;
+            // 
+            // TimerText
+            // 
+            this.TimerText.AutoSize = true;
+            this.TimerText.Font = new System.Drawing.Font("Microsoft Sans Serif", 55F);
+            this.TimerText.ForeColor = System.Drawing.Color.White;
+            this.TimerText.Location = new System.Drawing.Point(12, 9);
+            this.TimerText.Name = "TimerText";
+            this.TimerText.Size = new System.Drawing.Size(235, 85);
+            this.TimerText.TabIndex = 1;
+            this.TimerText.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(400, 100);
+            this.Controls.Add(this.TimerText);
+            this.Controls.Add(this.DebugMsg);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label DebugMsg;
+        private System.Windows.Forms.Label TimerText;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Timers;
+
+
+namespace bruhsplit
+{
+    public partial class Form1 : Form
+    {
+        private bool dragging = false;
+        private Point dragPoint = new Point();
+        private string status = "None";
+        private double startTime = 0;
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+
+        private GlobalKeyboardHook _globalKeyboardHook;
+        private System.Timers.Timer aTimer;
+
+        private void setUpMovement(Control obj) {
+            obj.MouseUp += new MouseEventHandler(Form1_MouseUp);
+            obj.MouseDown += new MouseEventHandler(Form1_MouseDown);
+            obj.MouseMove += new MouseEventHandler(Form1_MouseMove);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            setUpMovement(this);
+            //setUpMovement(TimerText);
+            TopMost = true;
+            aTimer = new System.Timers.Timer(1000 / 60);
+            aTimer.Elapsed += UpdateFrame;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+            TimerText.AutoSize = true;
+            TimerText.Text = "im pau";
+            TimerText.Size = new Size(Width, Height);
+            //KeyPreview = true;
+
+            _globalKeyboardHook = new GlobalKeyboardHook();
+            _globalKeyboardHook.KeyboardPressed += OnKeyPressed;
+        }
+        private string formatTime(float time)
+        {
+            int min = (int)Math.Floor(time / 60);
+            int sec = (int)Math.Floor(time % 60);
+            int ms = (int)Math.Floor((time % 1) * 100);
+            return String.Format("{0}:{1:D2}.{2:D2}", min,sec,ms);
+        }
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left) { return; }
+            // DebugMsg.Text = "down";
+            dragging = true;
+            dragPoint = e.Location;
+        }
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left) { return; }
+            // DebugMsg.Text = "up";
+            dragging = false;
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (dragging) {
+                MoveWindow(Handle, Cursor.Position.X-dragPoint.X, Cursor.Position.Y - dragPoint.Y, Width,Height,true);
+            }
+        }
+
+        private void OnKeyPressed(object sender, GlobalKeyboardHookEventArgs e)
+        {
+            // EDT: No need to filter for VkSnapshot anymore. This now gets handled
+            // through the constructor of GlobalKeyboardHook(...).
+            if (e.KeyboardState == GlobalKeyboardHook.KeyboardState.KeyDown)
+            {
+                // Now you can access both, the key and virtual code
+                Keys loggedKey = e.KeyboardData.Key;
+                int loggedVkCode = e.KeyboardData.VirtualCode;
+                if (loggedKey.ToString() == "F1") {
+                    if (status == "Running") {
+                        status = "Paused";
+                    } else if (status == "Paused") {
+                        status = "None";
+                    } else if (status == "None") {
+                        status = "Running";
+                        startTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+                    }
+                }
+            }
+        }
+
+        private void UpdateFrame(object sender, ElapsedEventArgs e)
+        {
+            Invoke(new Action(() => {
+                TimerText.Location = new Point(Width - TimerText.Width-5, (100-85)/2);
+                if (status == "None") {
+                    TimerText.Text = "0:00.00";
+                    TimerText.ForeColor = Color.LightGray;
+                } else if (status == "Running") {
+                    TimerText.Text = formatTime((float)(DateTimeOffset.Now.ToUnixTimeMilliseconds()-startTime)/1000);
+                    TimerText.ForeColor = Color.Green;
+                } else if (status == "Paused") {
+                    TimerText.ForeColor = Color.Cyan;
+                }
+            }));
+        }
+    }
+}
